3b86e77562b66e8cdfc621c1f4ed324d1f3b875e
VictorEnache <victor22enache@gmail.com>
2022-05-20 12:38:56 +0200

    create array

--- snack_1/index.php
+++ snack_1/index.php
@@ -9,4 +9,24 @@ Olimpia Milano - Cantù | 55-60
 
 <?php
 
-$partite = [];
+$partite = [
+    'partita_1' => [
+        'sq_casa' => 'rimini',
+        'sq_ospite' => 'pesaro',
+        'punti_sq_casa' => '40',
+        'punti_sq_ospite' => '42'
+    ],
+    'partita_2' => [
+        'sq_casa' => 'roma',
+        'sq_ospite' => 'napoli',
+        'punti_sq_casa' => '60',
+        'punti_sq_ospite' => '53'
+    ],
+    'partita_3' => [
+        'sq_casa' => 'napoli',
+        'sq_ospite' => 'pesaro',
+        'punti_sq_casa' => '30',
+        'punti_sq_ospite' => '30'
+    ],
+
+];
