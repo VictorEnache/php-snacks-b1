1ed7db49d97c477d422b39cba8e9209c23659dd1
VictorEnache <victor22enache@gmail.com>
2022-05-20 12:51:34 +0200

    stampa a schermo del risultato

--- snack_1/index.php
+++ snack_1/index.php
@@ -10,19 +10,19 @@ Olimpia Milano - Cantù | 55-60
 <?php
 
 $partite = [
-    'partita_1' => [
+     [
         'sq_casa' => 'rimini',
         'sq_ospite' => 'pesaro',
         'punti_sq_casa' => '40',
         'punti_sq_ospite' => '42'
     ],
-    'partita_2' => [
+ [
         'sq_casa' => 'roma',
         'sq_ospite' => 'napoli',
         'punti_sq_casa' => '60',
         'punti_sq_ospite' => '53'
     ],
-    'partita_3' => [
+ [
         'sq_casa' => 'napoli',
         'sq_ospite' => 'pesaro',
         'punti_sq_casa' => '30',
@@ -30,3 +30,26 @@ $partite = [
     ],
 
 ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+for ($i = 0; $i < count($partite); ++$i){ ?>
+    <?php var_dump($partite[$i]["sq_casa"]) ?>
+    <p><?php echo $partite[$i]['sq_casa']?> - <?php echo $partite[$i]['sq_ospite']?> | <?php echo $partite[$i]['punti_sq_casa']?> - <?php echo $partite[$i]['punti_sq_ospite']?></p>
+    
+<?php } ?>
+?>
+
+</body>
+</html>
